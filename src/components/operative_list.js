805c1d0aafc5e8200fa457f071b2123db20a53ca
Legacy92 <colemancorey22@gmail.com>
2018-05-18 15:32:13 -0700

    Completed Basic pages and navbar

---
+++ src/components/operative_list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Secret Operative List</h1>
+        <ol>
+            <li>Corey</li>
+            <li>Sam</li>
+            <li>Mark</li>
+            <li>Brett</li>
+            <li>Andrew</li>
+            <li>Scott</li>
+            <li>Cody</li>
+        </ol>
+    </div>
+)
---
+++ src/components/secret_doc.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Top Secret Document</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, atque. Quo, eligendi!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        <p>Lorem ipsum dolor sit.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem.</p>
+    </div>
+)
